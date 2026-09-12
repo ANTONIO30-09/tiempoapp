c2c1e02bffe0cba86f9a171c744f9f15d9b90272
ANTONIO30-09 <antoniovicentegarciacorrales2@gmail.com>
2026-09-11 21:37:02 -0400

    feat: agregar páginas de home, registro, login y perfil

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormularioLogin from '../components/FormularioLogin';
+
+export default function Login() {
+  return (
+    <section className="py-8">
+      <FormularioLogin />
+    </section>
+  );
+}
